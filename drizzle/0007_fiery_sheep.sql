bb20f814c5a8bb8eb3913321fd9cfcf402a32fc7
jaksia <jakub.siagi@gmail.com>
2026-02-04 23:11:41 +0100

    feat: add more locations

---
+++ drizzle/0007_fiery_sheep.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO "locations" ("name", "content", "path", "is_static") VALUES
+	('Tam kde minule', 'na rovnakom mieste ako minule', '/sounds/activities/locations/same_as_before.mp3', true),
+	('Na izbách', 'na izbách', '/sounds/activities/locations/rooms.mp3', true);
+--> statement-breakpoint
