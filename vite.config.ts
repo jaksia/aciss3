ebac990fbafb8e1eac6a9475fb8ca03127891caf
Jakub Šiagi <jakub.siagi@gmail.com>
2026-07-21 18:01:19 +0200

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+				experimental: {
+					async: true
+				}
+			},
+
+			adapter: adapter(),
+
+			experimental: {
+				remoteFunctions: true
+			}
+		})
+	]
 });
